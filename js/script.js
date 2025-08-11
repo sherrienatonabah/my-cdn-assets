d0e4ad6d553e5ed7a5f19809648e097c0ea796a8
sherrienatonabah <sherrienatonabah@gmail.com>
2025-08-11 15:12:04 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Основные скрипты сайта
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('CDN файлы загружены успешно!');
+    
+    // Пример функции
+    function showMessage() {
+        alert('Привет от jsDelivr CDN!');
+    }
+});
