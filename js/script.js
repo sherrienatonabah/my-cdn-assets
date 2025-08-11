dfb1c8c0769420af7684e1a3c8c0d3c56f26ffaa
sherrienatonabah <sherrienatonabah@gmail.com>
2025-08-11 16:05:54 +0300

    Delete js/script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// Основные скрипты сайта
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('CDN файлы загружены успешно!');
-    
-    // Пример функции
-    function showMessage() {
-        alert('Привет от jsDelivr CDN!');
-    }
-});
